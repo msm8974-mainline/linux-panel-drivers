8b110c01d8a9d6348c1ea3edcd70e39a0b03699c
Luca Weiss <luca@z3ntu.xyz>
2022-11-22 23:14:43 +0100

    config/dtb: fairphone-fp2: Add otm1902b and s6d6fa1 panels

---
+++ config/fairphone-fp2.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# DTB: From fp2-A10-gms-22.10.0-rel.0-manual.zip
+
+OPTIONS=()
+PANELS=(
+	[otm1902b_1080p_cmd]="fairphone,fp2-panel-otm1902b"
+	[s6d6fa1_1080p_video]="fairphone,fp2-panel-s6d6fa1"
+)
