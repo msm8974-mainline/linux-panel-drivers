4326442b7fbad271faf1e6cb94ed285f1f00f5e4
Valeriy Klimin <vdos63@gmail.com>
2024-03-20 19:59:14 +0300

    config/dtb: sony-aries: add panels
    Signed-off-by: Valeriy Klimin <vdos63@gmail.com>

---
+++ config/sony-aries.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# DTB: From lineage-18.1-20240214-nightly-z3c-signed.zip
+
+OPTIONS=()
+PANELS=(
+	[novatek_jdi_720p_cmd_panel]="sony,novatek-jdi-720p-cmd"
+	[novatek_sharp_720p_cmd_panel]="sony,novatek-sharp-720p-cmd"
+)
